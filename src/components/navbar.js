67c6a803a38060f63234e47cd2616fb538e0413e
Satyam <iamsatyam760@gmail.com>
2022-06-22 00:28:20 +0530

    fixed some bugs for mobile view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -33,19 +33,19 @@ class Navbar extends Component {
                 </div>
 
                 <div className={this.state.menu ? "sideBar" : "sideBarIcons"}>
-                    <Link to="/" style={{ textDecoration: "none" }}> <div className="linkDiv"><i className="fa fa-home faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Dashboard</span></div></Link>
+                    <Link to="/" style={{ textDecoration: "none" }} > <div className="linkDiv" onClick={this.handleMenuClick}><i className="fa fa-home faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Dashboard</span></div></Link>
 
-                    <Link to="/customers" style={{ textDecoration: "none" }} > <div className="linkDiv"><i className="fa fa-users faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Customer</span></div></Link>
+                    <Link to="/customers" style={{ textDecoration: "none" }} > <div className="linkDiv"  onClick={this.handleMenuClick}><i className="fa fa-users faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Customer</span></div></Link>
 
-                    <Link to="/messages" style={{textDecoration:'none'}}>  <div className="linkDiv"><i className="fa fa-comment faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Messages</span></div></Link>
+                    <Link to="/messages" style={{textDecoration:'none'}}>  <div className="linkDiv"  onClick={this.handleMenuClick}><i className="fa fa-comment faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Messages</span></div></Link>
 
-                    <Link to="/settings" style={{ textDecoration: "none" }}> <div className="linkDiv"><i className="fa fa-cog faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Settings</span></div></Link>
+                    <Link to="/settings" style={{ textDecoration: "none" }}> <div className="linkDiv"  onClick={this.handleMenuClick}><i className="fa fa-cog faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Settings</span></div></Link>
 
-                    <Link to="/help" style={{ textDecoration: "none" }}>   <div className="linkDiv"><i className="fa fa-question faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Help</span></div></Link>
+                    <Link to="/help" style={{ textDecoration: "none" }}>   <div className="linkDiv"  onClick={this.handleMenuClick}><i className="fa fa-question faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Help</span></div></Link>
 
-                    <Link to="/password" style={{ textDecoration: "none" }}>  <div className="linkDiv"><i className="fa fa-lock faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Password</span></div></Link>
+                    <Link to="/password" style={{ textDecoration: "none" }}>  <div className="linkDiv"  onClick={this.handleMenuClick}><i className="fa fa-lock faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Password</span></div></Link>
 
-                    <Link to="/signout" style={{ textDecoration: "none" }}>   <div className="linkDiv"><i className="fa fa-sign-out  faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Sign Out</span></div></Link>
+                    <Link to="/signout" style={{ textDecoration: "none" }}>   <div className="linkDiv"  onClick={this.handleMenuClick}><i className="fa fa-sign-out  faLeft" aria-hidden="true"> </i><span className={this.state.links ? "" : "hide"}> Sign Out</span></div></Link>
 
 
                 </div>
